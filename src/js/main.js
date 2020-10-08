3d511200cf52356d4be3d3734fb477ce76506f82
dangxuanthanh <dangxuanthanh1605@gmail.com>
2020-10-08 09:43:01 +0700

    Up

--- src/js/main.js
+++ src/js/main.js
@@ -64,4 +64,71 @@ function swiperInit() {
             clickable: "true"
         }
     });
+
+    var MenuSwiper = new Swiper(".other_food .swiper-container", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        // autoplay: {
+        //     delay: 5000,
+        // },
+        navigation: {
+            nextEl: '.nav-arrow-next',
+            prevEl: '.nav-arrow-prev',
+        },
+        breakpoints: {
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 15
+            },
+            990: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            }
+        }
+    });
+
+    var AboutSwiper = new Swiper(".about_2 .swiper-container", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        // autoplay: {
+        //     delay: 5000,
+        // },
+        navigation: {
+            nextEl: '.nav-arrow-next',
+            prevEl: '.nav-arrow-prev',
+        },
+        breakpoints: {
+            576: {
+                slidesPerView: 1,
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 15
+            },
+            1024: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            }
+        }
+    });
+
+    // Menu
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
 }
+
